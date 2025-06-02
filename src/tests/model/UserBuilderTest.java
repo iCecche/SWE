6c55141b28a3bd2e2a2c5a48b488fd879f7dd8b6
Alessio Ceccherini <alessioceccherini03@gmail.com>
2025-06-02 15:03:06 +0200

    Feat: add tests for UserBuilder class

---
+++ src/tests/model/UserBuilderTest.java
@@ -0,0 +1,103 @@
+package tests.model;
+
+import main.model.User;
+import main.model.UserBuilder;
+import main.model.UserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserBuilderTest {
+
+    private UserBuilder builder;
+
+    @BeforeEach void setUp() {
+        builder = UserBuilder.create();
+    }
+
+    @Test
+    void create() {
+        assertNotNull(builder);
+        assertEquals(UserBuilder.class, builder.getClass());
+    }
+
+    @Test
+    void withId() {
+        builder.withId(1);
+
+        assertEquals(1, builder.build().getId());
+    }
+
+    @Test
+    void withUsername() {
+        builder.withUsername("username");
+
+        assertEquals("username", builder.build().getUsername());
+    }
+
+    @Test
+    void withPassword() {
+        builder.withPassword("password");
+
+        assertEquals("password", builder.build().getPassword());
+    }
+
+    @Test
+    void withRole() {
+        builder.withRole(UserRole.USER);
+
+        assertNotNull(builder.build().getRole());
+        assertEquals(UserRole.USER, builder.build().getRole());
+    }
+
+    @Test
+    void withNome() {
+        builder.withNome("nome");
+
+        assertEquals("nome", builder.build().getNome());
+    }
+
+    @Test
+    void withCognome() {
+        builder.withCognome("cognome");
+
+        assertEquals("cognome", builder.build().getCognome());
+    }
+
+    @Test
+    void withIndirizzo() {
+        builder.withIndirizzo("indirizzo");
+
+        assertEquals("indirizzo", builder.build().getIndirizzo());
+    }
+
+    @Test
+    void withCap() {
+        builder.withCap("50018");
+
+        assertEquals("50018", builder.build().getCap());
+    }
+
+    @Test
+    void withProvincia() {
+        builder.withProvincia("FI");
+
+        assertEquals("FI", builder.build().getProvincia());
+    }
+
+    @Test
+    void withStato() {
+        builder.withStato("Italia");
+
+        assertEquals("Italia", builder.build().getStato());
+    }
+
+    @Test
+    void build() {
+        User createdUser = builder.build();
+
+        assertEquals(User.class, createdUser.getClass());
+        assertNotNull(createdUser);
+    }
+}
