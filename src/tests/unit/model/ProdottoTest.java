cf862358419aa29b033fd03bac431499bc7f921d
Alessio Ceccherini <alessioceccherini03@gmail.com>
2025-06-07 13:20:41 +0200

    Feat: add tests for DettaglioOrdine e Prodotto model class

---
+++ src/tests/unit/model/ProdottoTest.java
@@ -0,0 +1,50 @@
+package tests.unit.model;
+
+import main.model.DeliveryStatus;
+import main.model.Ordine;
+import main.model.PaymentStatus;
+import main.model.Prodotto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdottoTest {
+
+    private Prodotto prodotto;
+
+    @BeforeEach
+    void setUp() {
+        prodotto = new Prodotto(1, "nome", "descrizione", 1, 1);
+    }
+
+    @Test
+    void getId() {
+        int id = prodotto.getId();
+        assertEquals(1, id);
+    }
+
+    @Test
+    void getName() {
+        String name = prodotto.getName();
+        assertEquals("nome", name);
+    }
+
+    @Test
+    void getDescription() {
+        String description = prodotto.getDescription();
+        assertEquals("descrizione", description);
+    }
+
+    @Test
+    void getPrice() {
+        int price = prodotto.getPrice();
+        assertEquals(1, price);
+    }
+
+    @Test
+    void getStock() {
+        int stock = prodotto.getStock();
+        assertEquals(1, stock);
+    }
+}
